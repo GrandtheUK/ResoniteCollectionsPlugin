adab9e6f4eb3baf58b9dc0ce1187c155c008c747
Grand <grand.uk@tutanota.com>
2025-10-03 17:44:38 +0100

    Added ReadSyncRef Node to Parsing

---
+++ CollectionsPlugin/ProtoFlux/Collections/Parsing/ReadSyncRef.cs
@@ -0,0 +1,20 @@
+using FrooxEngine;
+using FrooxEngine.ProtoFlux;
+using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+
+namespace CollectionsPlugin.ProtoFlux.Collections.Parsing;
+
+[NodeCategory("Collections/Parsing")]
+[NodeName("Read SyncRef")]
+[ContinuouslyChanging]
+public class ReadSyncRef<T> : ObjectFunctionNode<FrooxEngineContext,T>
+    where T: class, IWorldElement, new()
+{
+    public readonly ObjectInput<SyncRef<T>> Reference;
+    protected override T Compute(FrooxEngineContext context)
+    {
+        SyncRef<T> reference = Reference.Evaluate(context);
+        return reference == null ? default : reference.Target;
+    }
+}

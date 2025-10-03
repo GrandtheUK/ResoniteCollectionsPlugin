b01ee66c14668183443c8f56cf459836a699fa54
Grand <grand.uk@tutanota.com>
2025-10-03 14:50:36 +0100

    Added Sync<T> parsing node

---
+++ CollectionsPlugin/ProtoFlux/Collections/Parsing/ReadSync.cs
@@ -0,0 +1,17 @@
+using FrooxEngine;
+using FrooxEngine.ProtoFlux;
+using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+
+namespace CollectionsPlugin.ProtoFlux.Collections.Parsing;
+
+public class ReadSync<T> : ValueFunctionNode<FrooxEngineContext,T>
+    where T: unmanaged
+{
+    public readonly ObjectInput<Sync<T>> Reference;
+    protected override T Compute(FrooxEngineContext context)
+    {
+        Sync<T> reference = Reference.Evaluate(context);
+        return reference == null ? default : reference.Value;
+    }
+}

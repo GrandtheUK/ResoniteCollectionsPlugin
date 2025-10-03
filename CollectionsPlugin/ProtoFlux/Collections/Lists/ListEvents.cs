7d5ea43d11856ee94d31a41575b7b185061c0466
Grand <grand.uk@tutanota.com>
2025-10-03 15:01:21 +0100

    Reworked ListEvents to be a ObjectFunctionNode

--- CollectionsPlugin/ProtoFlux/Collections/Lists/ListEvents.cs
+++ CollectionsPlugin/ProtoFlux/Collections/Lists/ListEvents.cs
@@ -7,13 +7,13 @@ namespace CollectionsPlugin.ProtoFlux.Collections.Lists;
 
 [NodeCategory("Collections/Lists")]
 [NodeName("List Events")]
-public class ListEvents: VoidNode<FrooxEngineContext>
+public class ListEvents: ObjectFunctionNode<FrooxEngineContext,ISyncMember>
 {
     public Call OnAdded;
     public Call OnRemoved;
-    public readonly ObjectOutput<ISyncMember> Value;
     public readonly GlobalRef<ISyncList> List;
     private ObjectStore<ISyncList> _list;
+    private ObjectStore<ISyncMember> _elem;
 
     private void OnListChanged(ISyncList value, FrooxEngineContext context)
     {
@@ -22,6 +22,7 @@ public class ListEvents: VoidNode<FrooxEngineContext>
         {
             list.ElementsAdded -= ListOnElementsAdded;
             list.ElementsRemoved -= ListOnElementsRemoved;
+            list.Changed -= ValueOnChanged;
         }
 
         void ListOnElementsAdded(ISyncList syncList, int startIndex, int count)
@@ -29,7 +30,9 @@ public class ListEvents: VoidNode<FrooxEngineContext>
             for (int i = startIndex; i < count; i++)
             {
                 ISyncMember elem = syncList.GetElement(i);
-                Value.Write(elem, context);
+                _elem.Write(elem,context);
+                Compute(context);
+                _elem.Write(default,context);
             }
             OnAdded.Execute(context);
         }
@@ -39,25 +42,38 @@ public class ListEvents: VoidNode<FrooxEngineContext>
             for (int i = startIndex; i < count; i++)
             {
                 ISyncMember elem = syncList.GetElement(i);
-                Value.Write(elem, context);
+                _elem.Write(elem,context);
+                Compute(context);
+                _elem.Write(default,context);
             }
             OnRemoved.Execute(context);
+        }
+        
+        void ValueOnChanged(IChangeable obj)
+        {
+            _elem.Write((ISyncMember)obj,context);
+            Compute(context);
+            _elem.Write(default,context);
         }
 
         if (value != null)
         {
             value.ElementsAdded += ListOnElementsAdded;
             value.ElementsRemoved += ListOnElementsRemoved;
+            value.Changed += ValueOnChanged;
         }
         _list.Write(value,context);
     }
     public ListEvents()
     {
         List = new GlobalRef<ISyncList>(this,0);
-        Value = new ObjectOutput<ISyncMember>(this);
     }
 
     public ISyncList Read(FrooxEngineContext context) => List.Read(context);
 
     public bool Write(ISyncList value, FrooxEngineContext context) => List.Write(value, context);
+    protected override ISyncMember Compute(FrooxEngineContext context)
+    {
+        return _elem.Read(context);
+    }
 }

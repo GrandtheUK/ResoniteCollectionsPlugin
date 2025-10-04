9a4bbf33d1d4471c029a7dbb2c6995be13958927
Grand <grand.uk@tutanota.com>
2025-10-04 03:30:42 +0100

    Added List Insert Nodes

---
+++ CollectionsPlugin/ProtoFlux/Collections/Lists/Insert/InsertObjectValueListElement.cs
@@ -0,0 +1,26 @@
+using FrooxEngine;
+using FrooxEngine.ProtoFlux;
+using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+
+namespace CollectionsPlugin.ProtoFlux.Collections.Lists.Insert;
+
+public class InsertObjectValueListElement<T> : ActionNode<FrooxEngineContext>
+{
+    public readonly ObjectInput<SyncFieldList<T>> List;
+    public readonly ValueInput<int> Index;
+    public readonly ObjectInput<T> Value;
+    public Continuation OnFailure;
+    public Continuation OnSuccess;
+    public Continuation OnNotFound;
+    
+    protected override IOperation Run(FrooxEngineContext context)
+    {
+        SyncFieldList<T> list = List.Evaluate(context);
+        int i = Index.Evaluate(context);
+        if (list == null || i<0 ) return OnNotFound.Target;
+        if (i > list.Count) return OnFailure.Target;
+        list.Insert(i, Value.Evaluate(context));
+        return OnSuccess.Target;
+    }
+}
---
+++ CollectionsPlugin/ProtoFlux/Collections/Lists/Insert/InsertReferenceListElement.cs
@@ -0,0 +1,27 @@
+using FrooxEngine;
+using FrooxEngine.ProtoFlux;
+using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+
+namespace CollectionsPlugin.ProtoFlux.Collections.Lists.Insert;
+
+public class InsertReferenceListElement<T> : ActionNode<FrooxEngineContext>
+    where T: class, IWorldElement, new()
+{
+    public readonly ObjectInput<SyncRefList<T>> List;
+    public readonly ValueInput<int> Index;
+    public readonly ObjectInput<T> Value;
+    public Continuation OnFailure;
+    public Continuation OnSuccess;
+    public Continuation OnNotFound;
+    
+    protected override IOperation Run(FrooxEngineContext context)
+    {
+        SyncRefList<T> list = List.Evaluate(context);
+        int i = Index.Evaluate(context);
+        if (list == null || i<0 ) return OnNotFound.Target;
+        if (i > list.Count) return OnFailure.Target;
+        list.Insert(i, Value.Evaluate(context));
+        return OnSuccess.Target;
+    }
+}

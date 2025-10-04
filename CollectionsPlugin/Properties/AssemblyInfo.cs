c0777bd5e3df4bffaf06c88df4eca44ee48779d7
Grand <grand.uk@tutanota.com>
2025-10-04 04:33:57 +0100

    Fixed issue preventing plugin from being loaded

--- CollectionsPlugin/Properties/AssemblyInfo.cs
+++ CollectionsPlugin/Properties/AssemblyInfo.cs
@@ -1,10 +1,3 @@
-using System.Reflection;
 using Elements.Data;
 
 [assembly: DataModelAssembly(DataModelAssemblyType.Core)]
-[assembly: AssemblyTitle("ResoniteCollectionsPlugin")]
-[assembly: AssemblyDescription("A Plugin to add ProtoFlux collections to Resonite")]
-[assembly: AssemblyCompany("Grand")]
-[assembly: AssemblyCopyright("Copyright © 2025 Grand")]
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0")]

4f4d70203094082f92371990fe29461d58df50ab
Grand <grand.uk@tutanota.com>
2025-10-05 19:45:23 +0100

    Create the bag contained type in AppendBag instead of taking it as input

--- CollectionsPlugin/ProtoFlux/Collections/Bags/AppendBag.cs
+++ CollectionsPlugin/ProtoFlux/Collections/Bags/AppendBag.cs
@@ -11,19 +11,20 @@ public class AppendBag<T> : ActionNode<FrooxEngineContext>
     where T: class, ISyncMember, new()
 {
     public readonly ObjectInput<SyncBag<T>> Bag;
-    public readonly ObjectInput<T> Element;
+    public readonly ObjectOutput<T> Element;
 
     public Continuation OnSuccess;
     public Continuation OnFailed;
     protected override IOperation Run(FrooxEngineContext context)
     {
         SyncBag<T> bag = Bag.Evaluate(context);
-        T elem = Element.Evaluate(context);
         if (bag == null)
         {
             return OnFailed.Target;
         }
+        T elem = new T();
         bag.Add(context.World.ReferenceController.PeekID(), elem, true);
+        Element.Write(elem,context);
         return OnSuccess.Target;
     }
 }

b2af7fa7f148f392447c15f0f7601cae2dd74622
Grand <grand.uk@tutanota.com>
2025-10-09 00:12:29 +0100

    Removed Component and Field Reading nodes and components They are out off scope of this plugin and were just added to see if it could be done but aren't needed for collections

--- CollectionsPlugin/Components/Permissions/ComponentReadBlocker.cs
+++
@@ -1,9 +0,0 @@
-using FrooxEngine;
-
-namespace CollectionsPlugin.Components.Permissions;
-
-[Category("Permissions")]
-public class ComponentReadBlocker : Component
-{
-    public readonly Sync<bool> Active;
-}

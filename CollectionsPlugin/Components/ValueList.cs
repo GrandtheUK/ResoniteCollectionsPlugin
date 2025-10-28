76a804bbad071ab54f40e577f724fd558b94024b
Grand <grand.uk@tutanota.com>
2025-10-28 15:49:26 +0000

    Fixed ValueList not allowing Strings and Uris to be valid types

--- CollectionsPlugin/Components/ValueList.cs
+++ CollectionsPlugin/Components/ValueList.cs
@@ -5,7 +5,7 @@ namespace CollectionsPlugin.Components;
 [Category("Data")]
 [GenericTypes(GenericTypesAttribute.Group.EnginePrimitives)]
 public class ValueList<T> : Component
-    where T: unmanaged
+    where T: IEquatable<T?>
 {
     public readonly SyncFieldList<T> Values;
 }
